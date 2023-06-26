1d9977b9246e9d1fe38de51ba0816306aacc978a
Tkakishak <tkakishakd@mail.ru>
2023-06-26 21:23:05 +0300

    Initial commit

---
+++ src/main/java/com/HW062623/HW062623/Hw062623Application.java
@@ -0,0 +1,13 @@
+package com.HW062623.HW062623;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw062623Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw062623Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/HW062623/HW062623/Hw062623ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HW062623.HW062623;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw062623ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
